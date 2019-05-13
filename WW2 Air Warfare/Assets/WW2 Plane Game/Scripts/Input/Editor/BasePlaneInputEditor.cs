b8ddc51b4fdee50425347c9a7b4d564f203e5b99
Twilit <37117133+Twilit@users.noreply.github.com>
2019-05-13 23:39:42 +0100

    hope this works

---
+++ WW2 Air Warfare/Assets/WW2 Plane Game/Scripts/Input/Editor/BasePlaneInputEditor.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace PlaneFlight
+{
+    [CustomEditor(typeof(BasePlaneInput))]
+    public class BasePlaneInputEditor : Editor
+    {
+        #region Variables
+        private BasePlaneInput targetInput;
+        #endregion
+
+        #region BuiltIn Methods
+        private void OnEnable()
+        {
+            targetInput = (BasePlaneInput)target;
+        }
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            string debugInfo = "";
+            debugInfo += "Pitch: " + targetInput.Pitch + "\n";
+            debugInfo += "Roll: " + targetInput.Roll + "\n";
+            debugInfo += "Yaw: " + targetInput.Yaw + "\n";
+            debugInfo += "Throttle: " + targetInput.Throttle + "\n";
+            debugInfo += "Brake: " + targetInput.Brake + "\n";
+            debugInfo += "Flaps: " + targetInput.Flaps + "\n";
+
+            //Custom Editor Code
+            GUILayout.Space(20);
+            EditorGUILayout.TextArea(debugInfo, GUILayout.Height(80));
+            GUILayout.Space(20);
+
+            Repaint();
+        }
+        #endregion
+    }
+
+}
---
+++ WW2 Air Warfare/Assets/WW2 Plane Game/Scripts/Propeller/PlanePropeller.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PlaneFlight
+{
+    public class PlanePropeller : MonoBehaviour
+    {
+        #region Variables
+        [Header("Propeller Properties")]
+        public float minQuadRPMs = 300f;
+        public float minTextureSwapLevel2 = 700f;
+        public float minTextureSwapLevel3 = 1500f;
+        public GameObject mainProp;
+        public GameObject blurredProp;
+
+        [Header("Material Properties")]
+        public Material blurredPropMat;
+        public Texture2D blurLevel1;
+        public Texture2D blurLevel2;
+        public Texture2D blurLevel3;
+        #endregion
+
+        #region BuiltIn Methods
+        private void Start()
+        {
+            if (mainProp && blurredProp)
+            {
+                HandleSwapping(0f);
+            }
+        }
+        #endregion
+
+        #region Custom Methods
+        public void HandlePropeller(float currentRPM)
+        {
+            //Get the current degrees per second
+            float dps = ((currentRPM * 360f) / 60f) * Time.deltaTime;
+
+            //Rotate Propeller
+            transform.Rotate(Vector3.forward, dps);
+
+            //Handle Propeller Swapping
+            if (mainProp && blurredProp)
+            {
+                HandleSwapping(currentRPM);
+            }           
+        }
+
+        void HandleSwapping(float currentRPM)
+        {
+            if (currentRPM > minQuadRPMs)
+            {
+                blurredProp.gameObject.SetActive(true);
+                mainProp.gameObject.SetActive(false);
+
+                if (blurredPropMat && blurLevel1 && blurLevel2 && blurLevel3)
+                {
+                    if (currentRPM > minTextureSwapLevel3)
+                    {
+                        blurredPropMat.SetTexture("_MainTex", blurLevel3);
+                    }
+                    else if (currentRPM > minTextureSwapLevel2)
+                    {
+                        blurredPropMat.SetTexture("_MainTex", blurLevel2);
+                    }
+                    else
+                    {
+                        blurredPropMat.SetTexture("_MainTex", blurLevel1);
+                    }
+                }                 
+            }
+            else
+            {
+                blurredProp.gameObject.SetActive(false);
+                mainProp.gameObject.SetActive(true);
+            }
+        }
+        #endregion
+    }
+}
