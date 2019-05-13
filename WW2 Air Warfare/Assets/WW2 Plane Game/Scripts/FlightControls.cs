4ebe7803f7e11510a377a11177cce402b979da11
Twilit <37117133+Twilit@users.noreply.github.com>
2019-05-13 23:43:16 +0100

    Revert "hope this works"
    This reverts commit b8ddc51b4fdee50425347c9a7b4d564f203e5b99.

---
+++ WW2 Air Warfare/Assets/WW2 Plane Game/Scripts/FlightControls.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlightControls : MonoBehaviour
+{
+    float thrust;
+    float roll;
+    float pitch;
+    float yaw;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
