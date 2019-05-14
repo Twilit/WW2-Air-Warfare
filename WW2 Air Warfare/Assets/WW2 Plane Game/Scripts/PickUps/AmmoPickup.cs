735a058fb41610ce6fa78a5a577b4ed7870444a4
Twilit <kinlongsha@hotmail.co.uk>
2019-05-14 11:11:53 +0100

    Prototype 1

---
+++ WW2 Air Warfare/Assets/WW2 Plane Game/Scripts/PickUps/AmmoPickup.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoPickup : MonoBehaviour { 
+
+         void OnTriggerEnter(Collider collider)
+    {
+    //printing a statement to console for testing purposes
+    print("pick up");
+        //if the the object that colided with the health pick up has a player tag 
+        //then get the players health script and check to see if ther is one and add health
+        if (collider.gameObject)
+        {
+            //colider.GetComponent gets the health script from the player not the pickup
+            Ammo Ammo = collider.GetComponent<Ammo>();
+
+            if(Ammo!=null)
+            { 
+
+             Ammo.Change(-50);
+                Destroy(gameObject);
+}
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ WW2 Air Warfare/Assets/WW2 Plane Game/Scripts/PickUps/HealPickup.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealPickup : MonoBehaviour {
+    
+     void OnTriggerEnter(Collider collider)
+    {
+        //printing a statement to console for testing purposes
+        print("pick up");
+        //if the the object that colided with the health pick up has a player tag 
+        //then get the players health script and check to see if ther is one and add health
+        if (collider.gameObject)
+        {
+            //colider.GetComponent gets the health script from the player not the pickup
+            Health health = collider.GetComponent<Health>();
+            if(health!=null)
+            { 
+
+             health.Damage(-50);
+                Destroy(gameObject);
+            }
+        }
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
