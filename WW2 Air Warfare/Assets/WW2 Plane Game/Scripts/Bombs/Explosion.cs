455a13f4dd3775d52d5071d4f78c3360effc5606
Twilit <37117133+Twilit@users.noreply.github.com>
2019-05-20 13:57:33 +0100

    Added wingtip trails when top speed

--- WW2 Air Warfare/Assets/WW2 Plane Game/Scripts/Bombs/Explosion.cs
+++ WW2 Air Warfare/Assets/WW2 Plane Game/Scripts/Bombs/Explosion.cs
@@ -4,14 +4,27 @@ using UnityEngine;
 
 public class Explosion : MonoBehaviour
 {
+    private Collider[] entitiesWithInRange;
 
     void Start()
     {
-        
+        Invoke("DealDamage", 0.12f);
     }
 
     void Update()
     {
         
-    }    
+    }
+
+    void DealDamage()
+    {
+        entitiesWithInRange = Physics.OverlapSphere(transform.position, 4f);
+
+        foreach(Collider entity in entitiesWithInRange)
+        {
+            print(entity.name);
+            
+            //Deal Damage
+        }
+    }
 }
