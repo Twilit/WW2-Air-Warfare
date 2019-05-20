dc412d60ffa38f2f7ea3f76ff6c5fc62e9f35407
Twilit <37117133+Twilit@users.noreply.github.com>
2019-05-20 23:02:05 +0100

    crabbobeam

---
+++ WW2 Air Warfare/Assets/WW2 Plane Game/Scripts/Enemy/Beam.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Beam : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * 1000f);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            print("Hit");
+            other.GetComponent<Rigidbody>().AddForce(transform.forward);
+        }
+    }
+}
---
+++ WW2 Air Warfare/Assets/WW2 Plane Game/Scripts/Enemy/WaterLaser.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterLaser : MonoBehaviour
+{
+    public GameObject beam;
+    public Transform player;
+    public bool shootingBeam;
+    public float trackingSpeed = 10f;
+
+    void Start()
+    {
+        shootingBeam = true;
+        StartCoroutine("ShootLaser");
+    }
+
+    void Update()
+    {
+
+    }
+
+    IEnumerator ShootLaser()
+    {
+        while (shootingBeam)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(player.position - transform.position), Time.deltaTime * trackingSpeed);
+
+            Instantiate(beam, transform.position, transform.rotation);
+            yield return new WaitForSeconds(0.7f);
+        }
+    }
+}
