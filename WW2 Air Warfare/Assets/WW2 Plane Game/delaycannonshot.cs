a85844189d3f7f5a33d89d1032ba0ed916a35e02
nathanielMeakin <44022056+nathanielMeakin@users.noreply.github.com>
2019-05-21 10:24:20 +0100

    nat

---
+++ WW2 Air Warfare/Assets/WW2 Plane Game/delaycannonshot.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class delaycannonshot : MonoBehaviour
+{
+
+    ParticleSystem shot;
+    [SerializeField] GameObject Gun;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator Gunobject()
+    {
+     
+        yield return new WaitForSeconds(10);
+         Gun.SetActive(false);
+
+ 
+
+        
+       
+
+        
+    }
+    
+
+}
