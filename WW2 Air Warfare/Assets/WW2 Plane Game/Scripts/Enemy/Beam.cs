c67e473b9ac65b6a1a36cfedc09a4dfc84eeed44
Twilit <kinlongsha@hotmail.co.uk>
2019-05-22 15:47:58 +0100

    adjusted waterbeam

--- WW2 Air Warfare/Assets/WW2 Plane Game/Scripts/Enemy/Beam.cs
+++ WW2 Air Warfare/Assets/WW2 Plane Game/Scripts/Enemy/Beam.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class Beam : MonoBehaviour
 {
+    private Transform player;
+
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player").transform.root;
     }
 
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * 2300f);
+        transform.Translate(Vector3.forward * Time.deltaTime * 2800f);
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(player.position - transform.position), Time.deltaTime * 0.005f);
     }
 
     private void OnTriggerEnter(Collider other)
@@ -21,6 +24,7 @@ public class Beam : MonoBehaviour
             print("Hit");
             other.transform.root.GetComponent<Rigidbody>().AddForce(transform.forward*500000);
             other.transform.root.GetComponent<Rigidbody>().AddTorque(transform.up * 130000);
+            Destroy(gameObject);
         }
     }
 }
