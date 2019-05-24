a02c313e3dcff415e7d9ed18b77759ca62723f81
nathanielMeakin <44022056+nathanielMeakin@users.noreply.github.com>
2019-05-24 14:10:08 +0100

    nat

---
+++ WW2 Air Warfare/Assets/spwancrab.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spwancrab : MonoBehaviour
+{
+    [SerializeField] private Transform crab;
+    [SerializeField] private Transform spwanpoint;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        crab.transform.position = spwanpoint.transform.position;
+    }
+}
