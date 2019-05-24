c691d5d880a60074c1bf1e0212695e46c5d366ac
JClarke97 <44021611+JClarke97@users.noreply.github.com>
2019-05-24 14:19:19 +0100

    win/loes pannel

--- WW2 Air Warfare/Assets/WW2 Plane Game/Scripts/UI/GameManager.cs
+++ WW2 Air Warfare/Assets/WW2 Plane Game/Scripts/UI/GameManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 
 public class GameManager : MonoBehaviour {
@@ -10,12 +11,20 @@ public class GameManager : MonoBehaviour {
     public Text score;
     public Text playerHealthTxt;
     public Text timeTxt;
+    public float delay = 5;
+     public string NewLevel= "MainMenu";
+
+
 
     public bool won;
 
     //creating a reffrence to the winpanel and loespannel
-    public GameObject losePanel;
+    public GameObject LosePanel;
     public GameObject winPanel;
+
+    public GameObject HealthPanel;
+    public GameObject ThrottleSlider;
+    public GameObject AmmoCounter;
 
     public static int amountkilled;
 
@@ -32,6 +41,7 @@ public class GameManager : MonoBehaviour {
         amountkilled = 0;
 
         winPanel.SetActive(false);
+        LosePanel.SetActive(false);
 
 
 
@@ -46,5 +56,17 @@ public class GameManager : MonoBehaviour {
     public void ShowWinScreen()
     {
         winPanel.SetActive(true);
+        HealthPanel.SetActive(false);
+        ThrottleSlider.SetActive(false);
+        AmmoCounter.SetActive(false);
+
+        StartCoroutine(LoadLevelAfterDelay(delay));
+
+
+    }
+    IEnumerator LoadLevelAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(0);
     }
 }
